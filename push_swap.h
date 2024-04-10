20c69980ce0c1491aaa088fc8059951f6091e83c
Pablo Gonzalez Gonzalez <pablogon@c2r10s4.42malaga.com>
2024-04-10 21:42:39 +0200

    Correción

---
+++ push_swap.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pablogon <pablogon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 18:49:50 by pablogon          #+#    #+#             */
+/*   Updated: 2024/04/10 19:29:26 by pablogon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef struct s_stack_node
+{
+	int						value;
+	int						current_position;
+	int						final_index;
+	int						push_price;
+	bool					above_median;
+	bool					cheapest;
+	struct s_stack_node		*target_node;
+	struct s_stack_node		*next;
+	struct s_stack_node		*prev;
+}	t_stack_node;
+
+
+
+
+#endif
---
+++ stack_init.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pablogon <pablogon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 18:52:35 by pablogon          #+#    #+#             */
+/*   Updated: 2024/04/10 21:30:56 by pablogon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/* Atol, necesito chekear posibles desbordamientos convirtiendo 
+cada cadena en un valor largo */
+
+static long	ft_atol(const char *str)
+{
+	
+}
